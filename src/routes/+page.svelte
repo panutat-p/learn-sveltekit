45597be34bbaf17121ebea73f63818b6cb44b173
panutat.p <pp.panutat@gmail.com>
2024-12-25 16:05:28 +0700

    feat: basic reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang='ts'>
+	let name = 'John'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<h2>Hello { name.toUpperCase() }</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
