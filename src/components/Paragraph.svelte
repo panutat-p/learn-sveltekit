f3d016b9ff8696a54abf33565ecdc54e3493a53a
panutat.p <pp.panutat@gmail.com>
2025-01-06 00:01:46 +0700

    feat: render HTML blob

--- src/components/Paragraph.svelte
+++ src/components/Paragraph.svelte
@@ -1,9 +1,14 @@
+<script>
+  let s = `this string contains some <strong>HTML!!!</strong>`;
+</script>
+
 <p>This is a paragraph.</p>
+<p>{@html s}</p>
 
 <style>
   p {
     color: rgb(19, 180, 116);
     font-family: 'Comic Sans MS', cursive;
-    font-size: 2em;
+    font-size: 1.5em;
   }
 </style>
