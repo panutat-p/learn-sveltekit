41f367a31855b9dee1017ef176bef84396ddaa3f
panutat.p <pp.panutat@gmail.com>
2025-01-06 22:17:51 +0700

    feat: derived state

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  let numbers = $state([1, 2, 3, 4])
+  let numbers = $state([1, 2])
+  let total = $derived(numbers.reduce((t, n) => t + n, 0))
 
   function addNumber() {
     numbers.push(numbers.length + 1)
@@ -14,6 +15,7 @@
       <li>{number}</li>
     {/each}
   </ul>
+  <p>Total: {total}</p>
 </div>
 
 <style>
@@ -21,8 +23,13 @@
     margin: 20px;
     padding: 2px;
   }
+  p {
+    color: rgb(27, 50, 180);
+    font-family: 'Comic Sans MS', cursive;
+    font-size: 1.5em;
+  }
   button {
-    background-color: rgb(38, 28, 184);
+    background-color: rgb(27, 50, 180);
     color: white;
     border: none;
     padding: 10px 20px;
